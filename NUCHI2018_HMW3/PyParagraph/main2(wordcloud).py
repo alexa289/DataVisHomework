36621bb59befb020f09da2e77f2e337c105b4ad5
alexandriacabello <alexa.290@gmail.com>
2018-02-17 13:58:48 -0600

    Added another script with wordcloud generator

---
+++ NUCHI2018_HMW3/PyParagraph/main2(wordcloud).py
@@ -0,0 +1,130 @@
+#Import Modules
+import os
+import sys
+import re
+import matplotlib.pyplot as plt 
+from PIL import Image
+import numpy as np
+
+from wordcloud import WordCloud, STOPWORDS
+
+#Open txt file. "test.tx is the test from the homework readme sample.
+# Insert here the txt file path
+txtinput = os.path.join('raw_data', 'test.txt')
+txtoutput = os.path.join('raw_data', 'testoutput.txt')
+curdir = os.path.dirname('_file_')
+with open(txtinput, 'r') as file:
+    file_contents = file.read()
+    print(file_contents)
+
+    #print(word_length)
+    #print(word_list)
+    #print(len(word_list))
+
+    #Know how many characters are in the text
+    total_char = len(file_contents)
+
+    print("Paragraph Analysis")
+    print("------------------")
+    print('Approximate word count: ', len(file_contents.split()))
+    print('Approximate sentence count: ', file_contents.count('.'))
+
+    #Hardcode the extraction of the following special characters in the text[ .,-?!::'><()]
+    count1 = 0
+    count2 = 0
+    count3 = 0
+    count4 = 0
+    count5 = 0
+    count6 = 0
+    count7 = 0
+    count8 = 0
+    count9 = 0
+    count10 = 0
+    count11 = 0
+    count12 = 0
+    count13 = 0
+    count14 = 0
+
+    for char in file_contents:
+        if char == '.':
+            count1 += 1
+        if char == ',':
+            count2 += 1
+        if char == ' ':
+            count3 += 1
+        if char == '-':
+            count4 += 1
+        if char == '"':
+            count5 += 1
+        if char == '?':
+            count6 += 1
+        if char == '!':
+            count7 += 1
+        if char == ':':
+            count8 += 1
+        if char == ';':
+            count9 += 1
+        if char == "'":
+            count10 += 1
+        if char == ">":
+            count11 += 1
+        if char == "<":
+            count12 += 1
+        if char == "(":
+            count13 += 1
+        if char == ")":
+            count14 += 1
+
+    #calculate the amount of special characters in the text
+    total_spec_char = count1 + count2 + count3 + count4 + count5 + count6 + \
+        count7 + count8 + count9 + count10 + count11 + count12 + count13 + count14
+
+    #Take out the number of special characters in the text and print total letters
+    total_words_only = total_char - total_spec_char
+
+    #Calculate an approximate of letters in a word
+    print('Approximate letter count (per word): ',
+          total_words_only / len(file_contents.split()))
+    #Calculate the lenght of a sentence. Assuming each sentence ends in '.'
+    print('Average sentence length (in words): ', len(
+        file_contents.split())/file_contents.count('.'))
+
+    print('----------------------------------------')
+    #Method 2 ---------------------
+    #I could accomplish something similar using regular expressions:
+
+    #I wasn't able to use this expresion, so I created my own. word_list = re.split("(?&lt;=[.!?]) +", file_contents)
+    word_list = re.split(
+        '\`|\~|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\+|\=|\[|\{|\]|\}|\||\\|\<|\,|\.|\>|\?|\/|\|\:|-|\s', file_contents)
+    #This expresion only considers blank spaces ->
+
+    #word_list = re.split(" +" , file_contents)
+
+    word_length = [len(x) for x in word_list]
+    print(word_length)
+    print("total words without special characters:", sum(word_length))
+    #Compare to the resut in Method 1 with the hardcoded special characters
+    print("total words without special characters", total_words_only)
+
+sys.stdout = open(txtoutput, 'w')
+print("Paragraph Analysis")
+print("------------------")
+print('Approximate word count: ', len(file_contents.split()))
+print('Approximate sentence count: ', file_contents.count('.'))
+print('Approximate letter count (per word): ',
+      total_words_only / len(file_contents.split()))
+print('Average sentence length (in words): ', len(
+    file_contents.split())/file_contents.count('.'))
+
+def create_wordcloud(text):
+    twit_mask = np.array(Image.open(
+        os.path.join(curdir, "twitter_mask.png")))
+
+    stopword = set(STOPWORDS)
+
+    wc = WordCloud(background_color = "white", mask = twit_mask, max_words = 600, stopwords = stopword)
+    wc.generate(text)
+    wc.to_file(os.path.join(curdir, "wc.png"))
+
+create_wordcloud(file_contents)
+
