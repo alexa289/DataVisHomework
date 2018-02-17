59d4e2ddcbd0a3c6b9c0aea8adb9d82bc04da356
alexandriacabello <alexa.290@gmail.com>
2018-02-17 12:53:17 -0600

    Modified file

--- NUCHI2018_HMW3/PyParagraph/main.py
+++ NUCHI2018_HMW3/PyParagraph/main.py
@@ -1,6 +1,7 @@
 #Import Modules
 import os
 import sys
+import re
 #Open txt file. "test.tx is the test from the homework readme sample.
 # Insert here the txt file path
 txtinput = os.path.join('raw_data', 'test.txt')
@@ -9,7 +10,15 @@ txtoutput = os.path.join('raw_data', 'testoutput.txt')
 with open(txtinput, 'r') as file:
     file_contents = file.read()
     print(file_contents)
-    total_char =len(file_contents)
+    
+    
+    #print(word_length)
+    #print(word_list)
+    #print(len(word_list))
+    
+    #Know how many characters are in the text
+    total_char = len(file_contents)
+    
     print("Paragraph Analysis")
     print("------------------")
     print('Approximate word count: ', len(file_contents.split()))
@@ -64,13 +73,30 @@ with open(txtinput, 'r') as file:
     #calculate the amount of special characters in the text
     total_spec_char = count1 + count2 + count3 + count4 + count5 + count6 + count7 + count8 + count9 + count10 + count11 + count12 + count13 + count14
     
-    #Take out the number of special characters from the total characters
+    #Take out the number of special characters in the text and print total letters
     total_words_only = total_char - total_spec_char 
     
+
     #Calculate an approximate of letters in a word 
     print('Approximate letter count (per word): ', total_words_only /len(file_contents.split()))
     #Calculate the lenght of a sentence. Assuming each sentence ends in '.'
     print('Average sentence length (in words): ', len(file_contents.split())/file_contents.count('.'))
+    
+    print('----------------------------------------')
+    #Method 2 ---------------------
+    #I could accomplish something similar using regular expressions:
+
+    #I wasn't able to use this expresion, so I created my own. word_list = re.split("(?&lt;=[.!?]) +", file_contents)
+    word_list = re.split('\`|\~|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\+|\=|\[|\{|\]|\}|\||\\|\<|\,|\.|\>|\?|\/|\|\:|-|\s', file_contents)
+    #This expresion only considers blank spaces -> 
+    
+    #word_list = re.split(" +" , file_contents)
+
+    word_length = [len(x) for x in word_list]
+    print(word_length)
+    print("total words without special characters:", sum(word_length))
+    #Compare to the resut in Method 1 with the hardcoded special characters
+    print("total words without special characters", total_words_only)
 
 sys.stdout = open(txtoutput, 'w')
 print("Paragraph Analysis")
