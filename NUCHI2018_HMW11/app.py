fc52d64affe358e5cdf8958b7f7125c9c2732be9
alexandriacabello <alexa.290@gmail.com>
2018-05-04 21:38:56 -0500

    set up the Flask

---
+++ NUCHI2018_HMW11/app.py
@@ -0,0 +1,33 @@
+#import dependencies
+from flask import Flask, render_template, jsonify, redirect
+from flask_pymongo import PyMongo
+#import the solution in python
+import scrape_mars
+
+#create instance of Flask
+app = Flask(__name__)
+
+
+mongo = PyMongo(app)
+
+# create route that renders index.html
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data_dict = scrape_mars.scrape()
+    mars.update(
+        {},
+        mars_data_dict,
+        upsert=True
+    )
+    return redirect("http://localhost:5000/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
