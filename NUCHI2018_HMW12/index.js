24dafe3fa1c0ee734a82fc3aec6496c828179f84
alexandriacabello <alexa.290@gmail.com>
2018-05-09 23:49:48 -0500

    homework 12 added

--- NUCHI2018_HMW12/index.js
+++ NUCHI2018_HMW12/index.js
@@ -1,21 +1,21 @@
-//Reference the variables on the json file
-var $dateTime = document.querySelector("#datetime");
-var $city = document.querySelector("#city");
-var $state = document.querySelector('#state');
-var $country = document.querySelector('#country');
-var $shape = document.querySelector('#shape');
-var $durationMinutes = document.querySelector('#durationMinutes');
-var $comments = document.querySelector('#comments');
 //Reference the variable for tbody
-var $tbody = document.querySelector('tbody')
-//Reference the variables for the button
-// var $searchBtn = document.querySelector('#search');
-// var $resetBtn = document.querySelector('#reset');
+var $tbody = document.querySelector("tbody");
 
-// //Add event listener to the searchBtn, add the call to the handleSearchButtonClick
-// $searchBtn.addEventListener('click', handleSearchButtonClick);
-// //Add event listener to the resetBtn
-// $resetBtn.addEventListener('click', resetData);
+//Reference the variables on the json file
+var $dateTimeInput = document.querySelector("#datetime");
+var $cityInput = document.querySelector("#city");
+var $stateInput = document.querySelector('#state');
+var $countryInput = document.querySelector('#country');
+var $shapeInput = document.querySelector('#shape');
+
+//Reference the variables for the button
+var $searchBtn = document.querySelector("#search");
+var $resetBtn = document.querySelector("#reset");
+
+//Add event listener to the searchBtn, add the call to the handleSearchButtonClick
+$searchBtn.addEventListener('click', handleSearchButtonClick);
+//Add event listener to the resetBtn
+$resetBtn.addEventListener('click', resetData);
 
 //Set filtereredData and resetData to dataSet
 var resetData = dataSet;
@@ -33,32 +33,72 @@ function renderTable() {
         //Loop through each inputfield and create a new value in the cell for every field
         for (var j = 0; j<inputfields.length; j++) {
             var inputfield = inputfields[j];
-            var $cellValue = $row.insertCell(j);
-            $value.innerText = data[field];
+            var $cell = $row.insertCell(j);
+            $cell.innerText = data[inputfield];
         } 
     }
 }
 function handleSearchButtonClick(event) {
+
     // preventDefault to prevent page from refreshing
     event.preventDefault();
 
-    var filteredDate = $dateTime.value.trim();
+    var filteredDate = $dateTimeInput.value.trim();
     if (filteredDate != ""){
-        filteredData = dataSet.filter(function(data){
+        filteredData = filteredData.filter(function(data){
         var dateTimeData = data.datetime;
         return dateTimeData === filteredDate;
         });
     };
+    
+    var filteredCountry = $countryInput.value.trim().toLowerCase();
+    if (filteredCountry != "") {
+        filteredData = filteredData.filter(function (data) {
+            var countryData = data.country.toLowerCase();
+            return countryData === filteredCountry;
+        });
+    };
 
-    var filteredCity = $city.value.trim().toLowerCase();
-    if (filteredCity != "") {
+    var filteredState = $stateInput.value.trim().toLowerCase();
+    if (filteredState !="") {
         filteredData = filteredData.filter(function(data){
+            var stateData = data.state.toLowerCase();
+            return stateData === filteredState;
+        });
+    };
+    
+    var filteredCity = $cityInput.value.trim().toLowerCase();
+    if (filteredCity != "") {
+        filteredData = filteredData.filter(function (data) {
             var cityData = data.city.toLowerCase();
             return cityData === filteredCity;
         });
     }; 
 
-    
+    var filteredShape = $shapeInput.value.trim().toLowerCase();
+    if (filteredShape) {
+        filteredData = filteredData.filter(function(data){
+            var shapeData = data.shape.toLowerCase();
+            return shapeData === filteredShape;
+        });
+    };
 
-
+    renderTable();
 }
+
+    function resetData() {
+        filteredData = dataSet;
+        $dateTimeInput.value = "";
+        $cityInput.value = "";
+        $stateInput.value ="";
+        $countryInput.value="";
+        $shapeInput.value = "";
+        renderTable();
+    }
+
+    function resetForm(){
+        document.getElementById("UFOForm").reset();
+    }
+
+    // Render the table for the first time on page load
+    renderTable();
